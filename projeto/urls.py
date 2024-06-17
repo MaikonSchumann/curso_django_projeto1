fdff32d66371a0a6c2b2b84fdf1e130751e9715f
Maikon Schumann <maikon.schumann@gmail.com>
2024-06-17 19:22:36 +0100

    add the first view

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view(request):
+    return HttpResponse('Hello World, tudo certo?')
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]

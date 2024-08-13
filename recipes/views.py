3060e23f7212373aa79f90730e56fdf1d39fae04
Maikon Schumann <maikon.schumann@gmail.com>
2024-08-13 20:19:50 +0100

    done

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     # return HTTP response
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Maikon Schumann',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     # return HTTP response
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Luiz Otávio',
+        'recipe': make_recipe(),
     })
-    
+
